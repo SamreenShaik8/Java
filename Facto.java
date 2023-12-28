531da7ab5af519a2044ac67849831c891cc10d79
Shaik Bunglow Samreen <shaiksamreensam123@gmail.com>
2023-12-28 21:22:23 +0530

    main

--- Facto.java
+++ Facto.java
@@ -3,22 +3,24 @@ import java.util.*;
 public class Facto {
 
     public static void printFactorial(int n){
-    // loop
-    int factorial=1;
-    for(int i=n;i>=1;i--){
+        // if the number is negative then the loop
+        if(n<0){
+            System.out.println("Invalid");
+            return;
+        }
+        // loop
+       int factorial=1;
+       for(int i=n;i>=1;i--){
         factorial=factorial*i;
+       }
+       System.out.println(factorial);
+       return;
     }
-    System.out.println(factorial);
-    return;
-}
-
-
-public static void main (String[] args){
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-
-    printFactorial(n);
-}
+    public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printFactorial(n);
+    }
 }
     
 
---
+++ inverted_halfpyra.java
@@ -0,0 +1,5 @@
+public class inverted_halfpyra {
+    public static void main(String[] args) {
+        for(int i=n)
+    }
+}
