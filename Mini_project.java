23d3864a47d16dbddb212a52559e4449d505c0cd
Shaik Bunglow Samreen <shaiksamreensam123@gmail.com>
2024-01-01 11:31:53 +0530

    main

---
+++ Mini_project.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Mini_project {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int myNumber=(int)(Math.random()*100);
+        int userNumber=0;
+
+        do{
+            System.out.println("Guess my number(1-100):");
+            userNumber=sc.nextInt();
+            if(userNumber==myNumber){
+                System.out.println("WOHOO...CORRECT");
+                break;
+            }
+            else if(userNumber>myNumber){
+                System.out.println("Your umber is too large");
+            }
+            else{
+                System.out.println("Your number is too small");
+            }
+        }
+        while(userNumber>=0);
+    
+       System.out.println("My number was:");
+       System.out.println(myNumber);
+    }
+}
---
+++ excep.java
@@ -0,0 +1,13 @@
+public class excep {
+    public static void main(String[] args) {
+        int[] marks={90,87,85};
+        try{
+            System.out.println(marks[5]);
+        }
+        catch(Exception exception){
+            // do somthing after catching
+
+        }
+        System.out.println("Hi samreen");
+    }
+}
