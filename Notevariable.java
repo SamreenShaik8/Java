78e77dc31a004c1586643812a5065bed45e59843
Shaik Bunglow Samreen <shaiksamreensam123@gmail.com>
2024-01-30 18:47:14 +0530

    main

---
+++ Notevariable.java
@@ -0,0 +1,28 @@
+public class Notevariable {
+    public static void main(String[] args) {
+        // byte - 1 [-128 to 127]
+        byte age = 40;
+        // short - 2
+        Short s=44;
+        // int - 4 -> 1,2,3...
+        int phno=1234352880;
+        // long - 8
+        long phno2=12345678900L;
+        // float - 4 ->4.5
+        float pi=3.14F;
+        // double - 8
+        // char - 2
+        char letter ='A';
+        // boolean - 1 ->true/false
+        boolean isAdult=true;
+
+        // printing all the data types
+        System.out.println(age);
+        System.out.println(s);
+        System.out.println(phno);
+        System.out.println(phno2);
+        System.out.println(pi);
+        System.out.println(letter);
+        System.out.println(isAdult);
+    }
+}
---
+++ e9.java
@@ -0,0 +1,6 @@
+public class e9{
+    public static void main(String[] args) {
+        String name="Samreen";
+        System.out.println(name);
+    }
+}
